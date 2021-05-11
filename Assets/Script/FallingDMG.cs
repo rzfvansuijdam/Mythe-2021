25f82f0a317add24d686a7feef6681aa7692e888
rzfvansuijdam <rzfvansuijdam@gmail.com>
2021-05-11 16:59:55 +0200

    added Health start

---
+++ Assets/Script/FallingDMG.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDMG : MonoBehaviour
+{
+    public float timeInAir = 5;
+
+    void Update()
+    {
+        //Here we decrease the time in air from 5
+        //Grounded is used by Rigidbody controller
+        if(!grounded)
+            timeInAir -= 1 * Time.deltaTime;
+   
+ 
+        //Increase the time in air to reach 5 each time we're on ground
+        if(grounded && timeInAir < 5 && timeInAir > 0)
+            timeInAir = 5;
+ 
+        //Making the player die when it reaches 0
+        if(timeInAir <= 0)
+            Debug.Log("You Died!");
+ 
+        //Or just damage player on a "checkpoint", in this case I use 3
+        if(timeInAir == 3 && grounded)
+            Health.Lives -= 30;
+ 
+    }
+}
---
+++ Assets/Script/Health.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public static float Lives = 100f;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            Debug.Log(Lives);
+        }
+    }
+}
