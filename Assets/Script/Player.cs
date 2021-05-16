ebc89cd98f3a736dab90a7a0bb04bca0580aece9
GodlyHamster <40576028+GodlyHamster@users.noreply.github.com>
2021-05-16 18:23:05 +0200

    basic movement and climbing

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    protected Rigidbody rb;
+
+    [SerializeField] protected GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Player_Climbing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Climbing : Player
+{
+
+    private bool _isAtVine = false;
+    private float _wallHeight = 0f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (_isAtVine && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine("climbWall", _wallHeight);
+        }
+    }
+
+    IEnumerator climbWall(float wallHeight)
+    {
+        wallHeight += 0.05f;
+        float climbTime = wallHeight / 3;
+
+        gameObject.GetComponent<Player_Movement>().enabled = false;
+        rb.useGravity = false;
+
+        for (int i = 0; i < 100; i++)
+        {
+            transform.position += new Vector3(0, wallHeight / 100, 0);
+            yield return new WaitForSeconds(climbTime / 100);
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            transform.position += transform.forward / 20;
+            yield return new WaitForSeconds(climbTime / 100);
+        }
+
+        gameObject.GetComponent<Player_Movement>().enabled = true;
+        rb.useGravity = true;
+
+        yield return null;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "Vine")
+        {
+            _isAtVine = true;
+            _wallHeight = collision.gameObject.transform.localScale.y;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        _isAtVine = false;
+        _wallHeight = 0f;
+    }
+}
