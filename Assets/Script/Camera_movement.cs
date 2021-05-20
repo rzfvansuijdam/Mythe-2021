c9c30577ac2bef2971cc55a4eefdc2952d7fac12
Ruben Dudink <rubendudink2003@gmail.com>
2021-05-20 09:20:43 +0200

    camera movement
    tada

--- Assets/Script/Camera_movement.cs
+++ Assets/Script/Camera_movement.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera_movement : MonoBehaviour
 {
+    public Camera camera;
+    public GameObject offset;
+    float mouseX;
+    float mouseY;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        offset.transform.Rotate(0, mouseX, 0);
+        camera.transform.Rotate(-mouseY, 0, 0);
+      //  if(mouse)
     }
+
 }
