6d2e87b2241101a432fe03fdd2a42e79f9f7bd20
GodlyHamster <40576028+GodlyHamster@users.noreply.github.com>
2021-06-16 16:44:07 +0200

    progress on main build

---
+++ Assets/Script/Misc/PlayerLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+
+    public static Vector3 playerSpawnPos;
+
+    void Start()
+    {
+        GameObject _player = (GameObject)Resources.Load("Prefabs/Player_old");
+
+        Instantiate(_player, playerSpawnPos, Quaternion.identity);
+    }
+}
---
+++ Assets/Script/Player/Player_Teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Teleport : Player
+{
+
+    public static Dictionary<string, Vector3> TpSpots = new Dictionary<string, Vector3>
+    {
+        {"Castle_Front", new Vector3(0, 0, 0)},
+        {"Castle_Left", new Vector3(0, 0, 0)},
+        {"Castle_Right", new Vector3(0, 0, 0)}
+    };
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Teleporter")
+        {
+            PlayerLoader.playerSpawnPos = TpSpots[collision.gameObject.name];
+            SceneManager.LoadScene("Castle_Inside", LoadSceneMode.Single);
+        }
+    }
+}
